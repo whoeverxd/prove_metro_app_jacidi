2634ac8fc03982d08e5fad7735bd39a12b347a9e
whoeverxd <joshuamoises1995@gmail.com>
2025-08-26 17:14:48 -0400

    Update main.dart
    añadido on willpopscope para que la app no se cierre al darle al boton de atras

--- flutter_app/prove_metro_app/lib/main.dart
+++ flutter_app/prove_metro_app/lib/main.dart
@@ -7,7 +7,6 @@ import 'screens/all_activities_page.dart';
 import 'screens/user_activities_page.dart';
 import 'providers/activities_provider.dart';
 
-
 void main() {
   runApp(
     MultiProvider(
@@ -28,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Jacidi Gym',
-      theme:CustomThemeData,
+      theme: CustomThemeData,
       home: const HomePage(),
       debugShowCheckedModeBanner: false,
     );
@@ -56,26 +55,48 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'MIS ACTIVIDADES',
+  Future<bool> _onWillPop() async {
+    final shouldExit = await showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("¿Salir de la aplicación?"),
+        content: const Text("¿Estás seguro de que quieres salir?"),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text("Cancelar" , style: TextStyle(color: Colors.grey) ,)
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fitness_center),
-            label: 'TODAS',
-
-
-
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: const Text("Salir", style: TextStyle(color: Colors.black),)
           ),
         ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+      ),
+    );
+
+    return shouldExit ?? false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'MIS ACTIVIDADES',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.fitness_center),
+              label: 'TODAS',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
